e8b0e61bd98c4fc9490aa5b285048246c89d527c
michelecornelli <m.cornelli3@campus.unimib.it>
2018-11-29 16:32:58 +0100

    Controllo count != 0 line 342

--- mr/src/main/java/org/apache/mahout/cf/taste/impl/eval/OrderBasedRecommenderEvaluator.java
+++ mr/src/main/java/org/apache/mahout/cf/taste/impl/eval/OrderBasedRecommenderEvaluator.java
@@ -339,7 +339,8 @@ public final class OrderBasedRecommenderEvaluator {
         }
       }
       if (vectorZ[i] != 0) {
-        ranks[i] = (rank / count) * (vectorZ[i] < 0 ? -1 : 1);  // better be at least 1
+    	  if(count != 0)
+    		  ranks[i] = (rank / count) * (vectorZ[i] < 0 ? -1 : 1);  // better be at least 1
         ranksAbs[i] = Math.abs(ranks[i]);
       }
     }
